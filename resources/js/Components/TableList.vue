4aa19a526968ee53e37a0baaf918b1206a6dcbed
AriqNaufalF <ariqn55@gmail.com>
2022-11-27 10:51:47 +0700

    make table as a component

---
+++ resources/js/Components/TableList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="p-6 bg-white shadow-lg rounded-lg">
+        <slot />
+        <div class="overflow-x-auto">
+            <table class="w-full text-sm">
+                <thead class="font-medium uppercase border-b-[3px] border-primary-800">
+                    <tr>
+                        <slot name="thead" />
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <slot name="tbody" />
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
