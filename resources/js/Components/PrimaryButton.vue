dce02cb28540d251216f2c0d4024911079a38e7c
AriqNaufalF <ariqn55@gmail.com>
2022-11-27 10:14:12 +0700

    edit style on hover

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -8,10 +8,8 @@ defineProps({
 </script>
 
 <template>
-    <button
-        :type="type"
-        class="py-1.5 text-sm font-medium bg-primary-600 text-white rounded-md shadow-md transition-transform hover:scale-105"
-    >
+    <button :type="type"
+        class="py-1.5 text-sm font-medium bg-primary-600 text-white rounded-md shadow-md transition-transform hover:scale-105 hover:bg-primary-800">
         <slot />
     </button>
 </template>
--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -8,17 +8,8 @@ defineProps({
 </script>
 
 <template>
-    <button
-        :type="type"
-        class="py-1.5 text-sm font-medium bg-secondary-400 text-black rounded-md hover:shadow-lg active:bg-secondary-500"
-    >
+    <button :type="type"
+        class="py-1.5 text-sm font-medium bg-secondary-400 text-black rounded-md shadow-md transition-transform hover:scale-105 hover:bg-secondary-600">
         <slot />
     </button>
 </template>
-
-<style scoped>
-button {
-    box-shadow: inset 1px -3px 6px -2px theme('colors.secondary.500'),
-        inset -1px 0 6px -2px theme('colors.secondary.500');
-}
-</style>
