ad4058dc3c5d97bf96ae50dca3538870bc8be9d4
AriqNaufalF <ariqn55@gmail.com>
2022-12-10 16:41:35 +0700

    make dashboard view

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,53 @@
 <script setup>
+import ButtonLink from '@/Components/ButtonLink.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import { CategoryScale, Chart as ChartJs, LinearScale, LineElement, PointElement, Tooltip } from "chart.js";
+import { Line } from "vue-chartjs";
+
+ChartJs.register(LineElement, PointElement, LinearScale, CategoryScale, Tooltip);
+
+const daysInMonth = 31
+function printAllDays() {
+    let days = [];
+    for (let i = 1; i <= daysInMonth; i++) {
+        days.push(i);
+    }
+    return days;
+}
+
+const chartOptions = {
+    responsive: true,
+    borderColor: '#FF6392',
+    scales: {
+        x: {
+            title: {
+                display: true,
+                color: 'black',
+                text: 'Days'
+            },
+        },
+        y: {
+            title: {
+                display: true,
+                color: 'black',
+                text: 'Total Orders'
+            },
+        }
+    }
+};
+
+const monthlyOrder = {
+    labels: printAllDays(),
+    datasets: [
+        {
+            label: 'Total orders',
+            backgroundColor: '#FF3370',
+            data: [50, 5, 10, 20, 40, 45, 15]
+        }
+    ]
+};
+
 </script>
 
 <template>
@@ -8,13 +55,35 @@ import { Head } from '@inertiajs/inertia-vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+        <div class="py-10 px-6 mx-auto">
+            <div class="text-center">
+                <h2 class="font-bold text-primary-800 text-2xl uppercase xl:text-3xl">Welcome to Kumbah</h2>
+            </div>
+            <div class="flex flex-col mt-20 gap-12 xl:flex-row">
+                <!-- Today info -->
+                <section class="bg-white w-full max-w-md max-h-[432px]  xl:max-w-xs shadow-md rounded-lg">
+                    <div class="flex flex-col h-full p-6">
+                        <h3 class="font-semibold text-lg text-center uppercase">Today</h3>
+                        <div class="mt-4">
+                            <p class="font-medium text-sm">Incoming Orders</p>
+                            <p class="font-semibold text-2xl">15</p>
+                        </div>
+                        <div class="mt-8">
+                            <p class="font-medium text-sm">Total Income</p>
+                            <p class="font-semibold text-2xl">Rp 29.000</p>
+                        </div>
+                        <ButtonLink
+                            class="flex justify-center items-center mt-4 w-full h-full max-h-36 font-medium text-lg bg-primary-600 text-white rounded-md transition-all hover:bg-primary-800 xl:mt-auto"
+                            :href="route('addOrder')" :custom="true" text="Add Order" />
                     </div>
-                </div>
+                </section>
+                <!-- monthly chart -->
+                <section class="bg-white w-full shadow-md rounded-lg">
+                    <div class="p-6">
+                        <h3 class="mb-4 font-semibold text-lg text-center uppercase">Order Statistic For The Month</h3>
+                        <Line :data="monthlyOrder" :options="chartOptions" />
+                    </div>
+                </section>
             </div>
         </div>
     </AuthenticatedLayout>
